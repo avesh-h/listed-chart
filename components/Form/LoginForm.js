fd0d5b514fd8d4b91d9487020bc6b77da1ab25a6
avesh <aveshhasanfatta1155@gmail.com>
2023-05-20 16:10:10 +0530

    final push

--- components/Form/LoginForm.js
+++ components/Form/LoginForm.js
@@ -109,9 +109,9 @@ export default function LoginForm() {
         </Button>
       </form>
       <Typography color="gray" className="mt-4 text-center font-normal">
-        Don't have an Account?{" "}
+        Don't have an Account?
         <a
-          href="#"
+          href="/"
           className="font-medium text-blue-500 transition-colors hover:text-blue-700"
         >
           Sign up
